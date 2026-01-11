0ac9372ec53521ae5bc0bc72584815dff00dc4f6
Franco Sarabia <franco.gb17@gmail.com>
2026-01-11 14:26:01 -0300

    book catalog functions added

--- src/app/book/pages/book-catalog-page/book-catalog-page.ts
+++ src/app/book/pages/book-catalog-page/book-catalog-page.ts
@@ -1,13 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 
 import { BookCatalogService } from '../../services/book-catalog.service';
 import { IBook } from '../../interfaces/book.interface';
 import { FavoriteBookService } from '../../services/favorite-book.service';
 import { STORAGE_KEY } from '../../../shared/consts/storage.const';
-import { Router } from '@angular/router';
-import { IFavoriteBookResponse } from '../../interfaces/favorite-book-response.interface';
+import { IFavoriteBook } from '../../interfaces/favorite-book.interface';
 
 @Component({
   selector: 'app-book-catalog-page',
@@ -21,71 +21,127 @@ export class BookCatalogPage implements OnInit {
   private router = inject(Router);
 
   public favoriteExternalIds: string[] = [];
-  public favorites: IFavoriteBookResponse[] = [];
+  public favorites: IFavoriteBook[] = [];
   public searchForm!: FormGroup;
   public books: IBook[] = [];
+  public selectedBook?: IBook;
+  public isLoading: boolean = false;
 
   // Variables de paginación
   public currentPage: number = 1;
   public totalPages: number = 0;
-  public pageSize: number = 10;
+  public pageSize: number = 20;
   public totalItems: number = 0;
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    const saved = this.bookCatalogService.lastSearch;
     this.searchForm = this.fb.group({
-      title: ['', [Validators.required, Validators.minLength(3)]],
-      author: ['', [Validators.required, Validators.minLength(3)]],
+      title: [saved.title, [Validators.required, Validators.minLength(3)]],
+      author: [saved.author, [Validators.required, Validators.minLength(3)]],
     });
 
-    const userData = localStorage.getItem(STORAGE_KEY);
-    if (userData) {
-      const userId = JSON.parse(userData).id;
+    this.loadState();
+    this.loadBooks();
+  }
 
-      this.favoriteBookService.getFavoritesByUser(userId).subscribe((favs) => {
+  loadBooks() {
+    const userData = localStorage.getItem(STORAGE_KEY);
+    if (!userData) {
+      this.router.navigate(['/auth/login']);
+      return;
+    }
+    const userId = JSON.parse(userData).id;
+
+    this.favoriteBookService.getFavoritesByUser(userId).subscribe({
+      next: (favs: IFavoriteBook[]) => {
         this.favorites = favs;
         this.favoriteExternalIds = favs.map((f: any) => f.externalId);
-      });
-    } else this.router.navigate(['/auth/login']);
+      },
+      error: (err) => {
+        console.error('Error obteniendo favoritos', err);
+        localStorage.removeItem(STORAGE_KEY);
+        this.router.navigate(['/auth/login']);
+      },
+    });
+  }
+
+  loadState() {
+    // Recuperar resultados previos si existen
+    if (this.bookCatalogService.lastSearch.books.length > 0) {
+      this.books = this.bookCatalogService.lastSearch.books;
+      this.currentPage = this.bookCatalogService.lastSearch.currentPage;
+      this.totalPages = this.bookCatalogService.lastSearch.totalPages;
+      this.totalItems = this.bookCatalogService.lastSearch.totalItems;
+    }
+
+    console.log(this.currentPage);
+    console.log(this.totalPages);
+    console.log(this.totalItems);
   }
 
   searchBooks(page: number = 1) {
     const { title, author } = this.searchForm.value;
     this.currentPage = page;
 
+    this.isLoading = true;
+
     this.bookCatalogService.searchBooks(title, author, page, this.pageSize).subscribe({
       next: (data: any) => {
         this.books = data.items;
         this.totalItems = data.total;
         this.totalPages = Math.ceil(data.total / this.pageSize);
+
+        // GUARDAR ESTADO EN EL SERVICIO
+        this.bookCatalogService.lastSearch = {
+          title,
+          author,
+          books: this.books,
+          currentPage: this.currentPage,
+          totalPages: this.totalPages,
+          totalItems: this.totalItems,
+        };
+
+        this.getPagesArray(this.currentPage, this.totalPages);
+
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.isLoading = false;
+        Swal.fire('Error', 'No se pudo cargar la información buscada', err);
       },
     });
   }
 
   addToFavorites(book: IBook) {
     console.log('Libro añadido a favoritos:', book);
-    const userDataCustom = localStorage.getItem(STORAGE_KEY);
+    const userData = localStorage.getItem(STORAGE_KEY);
 
-    if (userDataCustom) {
-      const user = JSON.parse(userDataCustom);
-      const userId = user.id;
-      this.favoriteBookService.addFavorite(book, userId).subscribe({
-        next: (favorite) => {
-          if (!this.favoriteExternalIds.includes(book.externalId)) {
-            this.favoriteExternalIds.push(book.externalId);
-            this.favorites.push(favorite);
-          }
-          Swal.fire({
-            position: 'top-end',
-            icon: 'success',
-            title: `Libro ${favorite.title} añadido a favoritos`,
-            showConfirmButton: false,
-            timer: 2000,
-          });
-        },
-      });
-    } else this.router.navigate(['/auth/login']);
+    if (!userData) {
+      this.router.navigate(['/auth/login']);
+      return;
+    }
+    const user = JSON.parse(userData);
+    const userId = user.id;
+    this.favoriteBookService.addFavorite(book, userId).subscribe({
+      next: (favorite) => {
+        if (!this.favoriteExternalIds.includes(book.externalId)) {
+          this.favoriteExternalIds.push(book.externalId);
+          this.favorites.push(favorite);
+        }
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: `Libro ${favorite.title} añadido a favoritos`,
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      },
+      error: (err) => {
+        Swal.fire('Error', err.message ?? 'No se pudo agregar el favorito', 'error');
+      },
+    });
   }
 
   removeFavorite(book: IBook) {
@@ -93,36 +149,78 @@ export class BookCatalogPage implements OnInit {
 
     const favoriteToRemove = this.favorites.find((fav) => fav.externalId === book.externalId);
 
-    if (favoriteToRemove) {
-      const favoriteId = favoriteToRemove.id;
-
-      this.favoriteBookService.removeFavorite(favoriteId).subscribe({
-        next: (success) => {
-          if (success) {
-            this.favoriteExternalIds = this.favoriteExternalIds.filter(
-              (id) => id !== book.externalId
-            );
-            this.favorites = this.favorites.filter((fav) => fav.id !== favoriteToRemove.id);
-            Swal.fire({
-              position: 'top-end',
-              icon: 'info',
-              title: 'Quitado de favoritos',
-              showConfirmButton: false,
-              timer: 2000,
-            });
-          } else {
-            Swal.fire('Error', 'No se pudo eliminar el favorito del servidor', 'error');
-          }
-        },
-      });
+    if (!favoriteToRemove) {
+      Swal.fire('Error', 'No se pudo agregar eliminar de favoritos', 'error');
+      return;
     }
+
+    const favoriteId = favoriteToRemove.id;
+
+    this.favoriteBookService.removeFavorite(favoriteId).subscribe({
+      next: (success) => {
+        if (success) {
+          this.favoriteExternalIds = this.favoriteExternalIds.filter(
+            (id) => id !== book.externalId
+          );
+          this.favorites = this.favorites.filter((fav) => fav.id !== favoriteToRemove.id);
+          Swal.fire({
+            position: 'top-end',
+            icon: 'info',
+            title: 'Quitado de favoritos',
+            showConfirmButton: false,
+            timer: 2000,
+          });
+        }
+      },
+      error: (err) => {
+        Swal.fire('Error', err.message ?? 'No se pudo eliminar de favoritos', 'error');
+      },
+    });
+  }
+
+  logout() {
+    Swal.fire({
+      title: '¿Cerrar sesión?',
+      text: 'Tendrás que volver a ingresar para ver tus favoritos.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3e4684',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, salir',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // 1. Limpiar el localStorage
+        localStorage.removeItem(STORAGE_KEY);
+
+        // 2. LIMPIAR EL ESTADO DEL SERVICIO (Importante para tu persistencia)
+        this.bookCatalogService.lastSearch = {
+          title: '',
+          author: '',
+          books: [],
+          currentPage: 1,
+          totalPages: 0,
+          totalItems: 0,
+        };
+
+        // 3. Redirigir al login
+        this.router.navigate(['/auth/login']);
+      }
+    });
+  }
+
+  openDetail(book: IBook) {
+    this.selectedBook = book;
   }
 
   // Helper para generar el arreglo de números de página en el HTML
-  getPagesArray(): number[] {
+  getPagesArray(currentPage?: number, totalPages?: number): number[] {
+    if (!currentPage) currentPage = this.currentPage;
+    if (!totalPages) totalPages = this.totalPages;
+
     const pages = [];
-    const start = Math.max(1, this.currentPage - 2);
-    const end = Math.min(this.totalPages, start + 4);
+    const start = Math.max(1, currentPage - 2);
+    const end = Math.min(totalPages, start + 4);
 
     for (let i = start; i <= end; i++) {
       pages.push(i);
--- src/app/book/services/book-catalog.service.ts
+++ src/app/book/services/book-catalog.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { IPaginatedBooks } from '../interfaces/paginated-books.interface';
 import { Observable } from 'rxjs';
+
+import { IPaginatedBooks } from '../interfaces/paginated-books.interface';
+import { environment } from '../../environment/environment';
+import { IBook } from '../interfaces/book.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -9,13 +12,26 @@ import { Observable } from 'rxjs';
 export class BookCatalogService {
   private httpClient = inject(HttpClient);
 
-  private apiUrl = 'http://localhost:5089/api';
+  // ESTADO PERSISTENTE
+  public lastSearch = {
+    title: '',
+    author: '',
+    books: [] as IBook[],
+    currentPage: 1,
+    totalPages: 0,
+    totalItems: 0,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  searchBooks(title: string, author: string, page: number, pageSize: number): Observable<IPaginatedBooks> {
+  searchBooks(
+    title: string,
+    author: string,
+    page: number,
+    pageSize: number
+  ): Observable<IPaginatedBooks> {
     return this.httpClient.get<IPaginatedBooks>(
-        `${this.apiUrl}/books/search?title=${title}&author=${author}&page=${page}&pageSize=${pageSize}`,
-    )
+      `${environment.apiUrl}/books/search?title=${title}&author=${author}&page=${page}&pageSize=${pageSize}`
+    );
   }
 }
